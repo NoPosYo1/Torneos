40e7198055ebdca80934eeaa189cb5f79760823c
franco.alfaro.flores@gmail.com <franco.alfaro.flores@gmail.com>
2026-04-07 10:08:22 -0400

    agregar keys

--- index.py
+++ index.py
@@ -2,8 +2,8 @@ import streamlit as st
 from supabase import create_client
 
 # Configuración de conexión (Usa tus credenciales de Supabase)
-url = "https://zokmyozaqhpmtwbtljsi.supabase.co"
-key = "o5Yz!u*u$CQqqT"
+url = st.secrets["SUPABASE_URL"]
+key = st.secrets["SUPABASE_KEY"]
 supabase = create_client(url, key)
 
 st.title("🎮 Panel de Gestión de Torneo")
