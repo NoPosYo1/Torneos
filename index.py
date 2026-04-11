7a24767548ef14e98ed7dc7f450a5401a4d80560
franco.alfaro.flores@gmail.com <franco.alfaro.flores@gmail.com>
2026-04-10 21:51:38 -0400

    Update index.py

--- index.py
+++ index.py
@@ -52,10 +52,14 @@ else:
 if len(equipos_db) > 0:
     for equipo in equipos_db:
         estado_jugador1 = "Activo" if equipo.get('jugador1', {}).get('estado_activo') is True else "AUSENTE"
-        estado_jugador2 = "Sin Duo"
+        
         try:
             estado_jugador2 = "Activo" if equipo.get('jugador2', {}).get('estado_activo') is True else "AUSENTE"
         except AttributeError:
             estado_jugador2 = "Sin Duo"
-        st.write(f"Equipo ID: {equipo['id']}. Estado: {equipo['estado_activo']}.Jugador 1: {equipo.get('jugador1', {}).get('nick', 'Sin nombre')} estado: {estado_jugador1}.Jugador 2: {equipo.get('jugador2', {}).get('nick', 'Sin Duo')} estado: {estado_jugador2}")
         
+        try:
+
+            st.write(f"Equipo ID: {equipo['id']}. Estado: {equipo['estado_activo']}.Jugador 1: {equipo.get('jugador1', {}).get('nick', 'Sin nombre')} estado: {estado_jugador1}.Jugador 2: {equipo.get('jugador2', {}).get('nick', 'Sin Duo')} estado: {estado_jugador2}")
+        except KeyError:
+            st.write(f"Equipo ID: {equipo['id']}. Estado: {equipo['estado_activo']}.Jugador 1: {equipo.get('jugador1', {}).get('nick', 'Sin nombre')} estado: {estado_jugador1}.Jugador 2: Sin Duo")
