ad323fb668bab7045a337cd93ffbea5cfc4b59c8
franco.alfaro.flores@gmail.com <franco.alfaro.flores@gmail.com>
2026-04-11 15:33:40 -0400

    Update admin_app.py

--- admin_app.py
+++ admin_app.py
@@ -77,6 +77,8 @@ else:
     st.sidebar.title("Menú de Administración")
     if st.button("🏠 IR A PANEL PRINCIPAL"):
         cambiar_vista('principal')
+    if st.sidebar.button("⚔️ IR A GESTIÓN DE EQUIPOS"):
+        cambiar_vista('reg_equipo')
     st.sidebar.markdown("- [Gestión de Equipos](#gestión-de-equipos)")
     if st.button("IR A REGISTRAR EQUIPO"):
         cambiar_vista('reg_equipo')
