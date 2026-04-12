f269e1cc9b6d4e1e6a348060d59b59404fb119fc
franco.alfaro.flores@gmail.com <franco.alfaro.flores@gmail.com>
2026-04-12 00:12:35 -0400

    Update admin_app.py

--- admin_app.py
+++ admin_app.py
@@ -7,7 +7,24 @@ supabd = create_client(st.secrets["SUPABASE_URL"], st.secrets["SUPABASE_KEY"])
 st.set_page_config(page_title="Torneo 2v2 GGReport - Admin", layout="wide", initial_sidebar_state="collapsed")
 
 st.session_state.logged_in = st.session_state.get('logged_in', False)
-
+st.markdown(
+        f"""
+            <style>
+            [data-testid="stSidebar"] {{
+                background-image: url("https://images6.alphacoders.com/909/909375.jpg");
+                background-size: cover !important;
+                background-position: center !important;
+                background-repeat: no-repeat !important;
+            }}
+            /* Esto hace que los widgets encima del fondo sean legibles */
+            [data-testid="stSidebar"] .stMarkdown, [data-testid="stSidebar"] .stButton {{
+                background-color: rgba(0, 0, 0, 0.4) !important; /* Fondo semi-transparente para botones */
+                border-radius: 10px !important;
+            }}
+            </style>
+            """,
+        unsafe_allow_html=True
+    )
 
 
 
@@ -138,24 +155,7 @@ else:
 
 
     st.sidebar.title("Reglas del Torneo")
-    st.markdown(
-            f"""
-            <style>
-            [data-testid="stSidebar"] {{
-                background-image: url("https://images6.alphacoders.com/909/909375.jpg");
-                background-size: cover !important;
-                background-position: center !important;
-                background-repeat: no-repeat !important;
-            }}
-            /* Esto hace que los widgets encima del fondo sean legibles */
-            [data-testid="stSidebar"] .stMarkdown, [data-testid="stSidebar"] .stButton {{
-                background-color: rgba(0, 0, 0, 0.4) !important; /* Fondo semi-transparente para botones */
-                border-radius: 10px !important;
-            }}
-            </style>
-            """,
-            unsafe_allow_html=True
-        )
+
     
     st.sidebar.title("Menú de Administración")
     if st.sidebar.button("🏠 IR A PANEL PRINCIPAL", key="btn_principal"):
